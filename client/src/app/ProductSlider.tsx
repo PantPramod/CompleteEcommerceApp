90ce0d4b01d8a585fa7ff1ebc3a3eea567b0a592
Pramod Pant <pramodpant100@gmail.com>
2023-05-09 14:40:29 +0530

    error resolved

--- client/src/app/ProductSlider.tsx
+++ client/src/app/ProductSlider.tsx
@@ -1,45 +1,47 @@
 "use client"
-import  { useRef } from 'react'
+import { useRef } from 'react'
 
-type propTypes={
-  heading?:string
+type propTypes = {
+  heading?: string
 }
-const ProductSlider = ({heading}:propTypes) => {
-const scrollBarRef = useRef<any>();
-const scrollContainerRef = useRef<any>();
+const ProductSlider = ({ heading }: propTypes) => {
+  const scrollBarRef = useRef<any>();
+  const scrollContainerRef = useRef<any>();
   return (
-    <div 
-    ref={scrollContainerRef}
-    className='relative  my-10  bg-white '>
+    <div
+      ref={scrollContainerRef}
+      className='relative  my-10  bg-white '>
       <p className='font-bold text-left ml-10 text-2xl pt-4'>{heading}</p>
-    <div 
-    ref={scrollBarRef}
-    className='flex overflow-x-scroll scroll-hide'>
-      <div 
-      onClick={()=>scrollBarRef.current.scrollLeft-=window.innerWidth}
-      className='absolute bg-[rgba(245, 240, 240, 0.301)] hover:bg-black w-[50px] left-0 top-0 bottom-0 text-white flex items-center justify-center cursor-pointer ' >
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="w-10 h-10">
-          <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
-        </svg>
+      <div
+        ref={scrollBarRef}
+        className='flex overflow-x-scroll scroll-hide'>
+        <div
+          onClick={() => scrollBarRef.current.scrollLeft -= window.innerWidth}
+          className='absolute bg-[rgba(245, 240, 240, 0.301)] hover:bg-black w-[50px] left-0 top-0 bottom-0 text-white flex items-center justify-center cursor-pointer ' >
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="w-10 h-10">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
+          </svg>
+        </div>
+        <div
+          onClick={() => scrollBarRef.current.scrollLeft += window.innerWidth}
+          className='absolute bg-[rgba(245, 240, 240, 0.301)] hover:bg-black w-[50px] right-0 top-0 bottom-0 text-white flex items-center justify-center cursor-pointer transition-all ease-in-out duration-300' >
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="w-10 h-10">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+          </svg>
+        </div>
+        {
+          arr.map((product,index) => <div 
+          key={index}
+          className=''>
+            <Product
+              imgSrc={product.imgSrc}
+              name={product.name}
+              price={product.price}
+              desc={product.desc}
+            />
+          </div>)
+        }
       </div>
-      <div 
-      onClick={()=>scrollBarRef.current.scrollLeft+=window.innerWidth}
-      className='absolute bg-[rgba(245, 240, 240, 0.301)] hover:bg-black w-[50px] right-0 top-0 bottom-0 text-white flex items-center justify-center cursor-pointer transition-all ease-in-out duration-300' >
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="w-10 h-10">
-          <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
-        </svg>
-      </div>
-      {
-        arr.map((product) => <div className=''>
-          <Product
-            imgSrc={product.imgSrc}
-            name={product.name}
-            price={product.price}
-            desc={product.desc}
-          />
-        </div>)
-      }
-    </div>
     </div>
   )
 }
